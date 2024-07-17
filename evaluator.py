382cd8cd61045fc9e84fad5e1e525e7537a0fa9f
jariasca9 <m212t0436@misantla.tecnm.mx>
2024-07-17 01:08:48 -0600

    read_pdf function modified

--- evaluator.py
+++ evaluator.py
@@ -2,16 +2,18 @@ from lark import Transformer
 import yaml
 
 class Evaluator(Transformer):
-    def start(self, expression):
+    def start(self, expression) -> str:
         return expression
     
-    def expression(self, commands):
+    def expression(self, commands) -> str:
         return commands
     
-    def commands(self, command):
+    def commands(self, command) -> str:
         return command
     
-    def read_pdf(self, filename):
+    def read_pdf(self, filename) -> None:
+        filename = filename[0]
+        
         # Implementar la lógica para leer el pdf
         
         # Diccionario (ficticio) obtenido del texto del pdf
@@ -25,13 +27,12 @@ class Evaluator(Transformer):
         # Convertir el diccionario a formato yaml
         yaml_data = yaml.dump(cv_data,default_flow_style=False, allow_unicode=True)
         
+        # Guardar el archivo yaml
         try:
-            with open('cv.yaml', 'w') as f:
+            with open(f'{filename.split(".pdf")[0]}_haki.yaml', 'w') as f:
                 f.write(yaml_data)
-            return True 
         except Exception as e:
-            print(f'Error: {e}')
-            return False
+            print(f'Error: {e}')    
     
-    def filename(self, filename):
-        return filename
+    def filename(self, tokens) -> str:
+        return tokens[0] + "." + tokens[1]
