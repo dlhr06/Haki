dd04d512ae306d879ed3032b100841820b4302a7
jariasca9 <m212t0436@misantla.tecnm.mx>
2024-07-16 20:29:57 -0600

    read_pdf fonction modified

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,5 @@
 from lark import Transformer
+import yaml
 
 class Evaluator(Transformer):
     def start(self, expression):
@@ -11,8 +12,26 @@ class Evaluator(Transformer):
         return command
     
     def read_pdf(self, filename):
-        # Implementar la lógica para leer el PDF
-        return f"Reading PDF: {filename}"
+        # Implementar la lógica para leer el pdf
+        
+        # Diccionario (ficticio) obtenido del texto del pdf
+        cv_data = {
+            "personal_information": {
+                "name": "Julian",
+                "age": 20
+            }
+        }
+
+        # Convertir el diccionario a formato yaml
+        yaml_data = yaml.dump(cv_data,default_flow_style=False, allow_unicode=True)
+        
+        try:
+            with open('cv.yaml', 'w') as f:
+                f.write(yaml_data)
+            return True 
+        except Exception as e:
+            print(f'Error: {e}')
+            return False
     
-    def filename(self, name, extension):
-        return name + '.' + extension
+    def filename(self, filename):
+        return filename
