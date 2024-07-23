35396ef5e880b321f9b0901e8eeb6d8ccbfe3547
jariasca9 <m212t0436@misantla.tecnm.mx>
2024-07-23 00:54:20 -0600

    language dictionary for Haki DSL

---
+++ language_dictionary.py
@@ -0,0 +1,65 @@
+description = {
+    'es': 'Identifica la información de tu Curriculum Vitae en formato PDF para facilitar la postulación a ofertas de trabajo donde se requiere completar detalles como educación, experiencia laboral, idiomas, etc.',
+    'en': 'Identify the information of your Curriculum Vitae in PDF format to facilitate the application to job offers where details such as education, work experience, languages, etc. are required.',
+    'fr': "Identifiez les informations de votre Curriculum Vitae au format PDF pour faciliter la candidature à des offres d'emploi où des détails tels que l'éducation, l'expérience professionnelle, les langues, etc. sont requis."
+}
+
+commands = {
+    'es': 'Comandos disponibles',
+    'en': 'Available commands',
+    'fr': 'Commandes disponibles'
+}
+
+command_read_pdf = {
+    'es': 'Comando para extraer información de un archivo PDF.',
+    'en': 'Command to extract information from a PDF file.',
+    'fr': 'Commande pour extraire des informations d\'un fichier PDF.'
+}
+
+command_get_professional_summary = {
+    'es': 'Comando para generar un resumen profesional a partir de un archivo YAML.',
+    'en': 'Command to generate a professional summary from a YAML file.',
+    'fr': 'Commande pour générer un résumé professionnel à partir d\'un fichier YAML.'
+}
+
+command_get_motivation_letter = {
+    'es': 'Comando para generar una carta de motivaciones a partir de un archivo YAML.',
+    'en': 'Command to generate a motivation letter from a YAML file.',
+    'fr': 'Commande pour générer une lettre de motivation à partir d\'un fichier YAML.'
+}
+
+command_apply_for_job = {
+    'es': 'Comando para aplicar a una oferta de trabajo. Requiere un archivo YAML y la dirección URL a la oferta de trabajo.',
+    'en': 'Command to apply for a job offer. Requires a YAML file and the URL address to the job offer.',
+    'fr': 'Commande pour postuler à une offre d\'emploi. Nécessite un fichier YAML et l\'adresse URL de l\'offre d\'emploi.'
+}
+
+arg_pdf_filename = {
+    'es': 'Nombre del archivo PDF que contiene la información del Curriculum Vitae.',
+    'en': 'Name of the PDF file that contains the Curriculum Vitae information.',
+    'fr': 'Nom du fichier PDF contenant les informations du Curriculum Vitae.'
+}
+
+arg_yaml_filename = {
+    'es': 'Nombre del archivo YAML que contiene la información del Curriculum Vitae.',
+    'en': 'Name of the YAML file that contains the Curriculum Vitae information.',
+    'fr': 'Nom du fichier YAML contenant les informations du Curriculum Vitae.'
+}
+
+arg_url = {
+    'es': 'Dirección URL de la oferta de trabajo.',
+    'en': 'URL address of the job offer.',
+    'fr': 'Adresse URL de l\'offre d\'emploi.'
+}
+
+arg_output_language = {
+    'es': 'Lenguaje del texto de salida. Por defecto es el idioma del sistema.',
+    'en': 'Output text language. By default it is the system language.',
+    'fr': 'Langue du texte de sortie. Par défaut, c\'est la langue du système.'
+}
+
+insuficient_args = {
+    'es': 'Se requieren al menos dos argumentos.',
+    'en': 'At least two arguments are required.',
+    'fr': 'Au moins deux arguments sont nécessaires.'
+}
